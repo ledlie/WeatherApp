2235e8836dfb3b57d6b069f726eb69d1c68b4ce3
Jonathan Ledlie <ledlie@cmtelematics.com>
2020-01-31 19:03:11 -0500

    Update json response - does not seem to match public spec

--- app/src/main/java/com/htboiz/weatherapp/WeatherResponse.java
+++ app/src/main/java/com/htboiz/weatherapp/WeatherResponse.java
@@ -5,25 +5,25 @@ import java.util.List;
 public class WeatherResponse {
 
     public final Coord coord;
-    public final List<WeatherDescription> weather;
-    public final WeatherDetails main;
+    public final List<WeatherDescription> list;
 
-    public WeatherResponse(Coord coord, List<WeatherDescription> weather, WeatherDetails main) {
+    public WeatherResponse(Coord coord, List<WeatherDescription> list) {
         this.coord = coord;
-        this.weather = weather;
-        this.main = main;
+        this.list = list;
     }
 
     @Override
     public String toString() {
+        WeatherDescription item = null;
+        if (list != null && list.size() > 0)
+            item = list.get(0);
+
         return "WeatherResponse{" +
                 "coord=" + coord +
-                ", weather=" + weather +
-                ", main=" + main +
+                ", item=" + item +
                 '}';
     }
 
-    // {"coord": { "lon": 139,"lat": 35},
     public class Coord {
         float lon;
         float lat;
@@ -31,13 +31,28 @@ public class WeatherResponse {
 
     public class WeatherDescription {
         int id;
-        String main;
-        String description;
+        WeatherDetails main;
+
+        @Override
+        public String toString() {
+            return "{" +
+                    "id=" + id +
+                    ", main=" + main +
+                    '}';
+        }
     }
 
     public class WeatherDetails {
         float temp;
         float feelsLike;
+
+        @Override
+        public String toString() {
+            return "{" +
+                    "temp=" + temp +
+                    ", feelsLike=" + feelsLike +
+                    '}';
+        }
     }
 
 }
